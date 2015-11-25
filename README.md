c327f619b62f5beddb33b21fb02e135016cfabb6
Tilman Holschuh <tilman@heroku.com>
2015-11-25 13:22:22 -0800

    Implement lager_elogfmt_formatter

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # lager_elogfmt_formatter
-A lager formatter for logfmt and structured logs
+
+A lager formatter for `logfmt` and `structured logs`.
+
+Leverages [lager](https://github.com/lager/lager.git) metadata to simplify
+logging of messages consumed, for example, by Splunk and Librato.
+
+Additional metadata passed to logging calls with lager (when using the lager
+parse transform) will be included in logs:
+
+```
+lager:error([{type, badarg}, {{count, "badarg"}, 1}], "failed to parse: ~w", [Input])
+```
+
+The output to this log call will include:
+
+```
+app=myapp application=my_api module=parser function=parse line=42
+severity=error type=badarg count#myapp.badarg=1 msg='failed to parse: ...'"
+```
+
+# Setup & Configuration
+
+Include `lager_elogfmt_formatter` in your `rebar.config`:
+
+```
+{deps, [
+    {lager_elogfmt_formatter,
+     {git, "https://github.com/heroku/lager_elogfmt_formatter.git", {tag, "1.0.0"}}}
+]}
+```
+
+Set `lager_elogfmt_formatter` as formatter for a lager handler. There are the
+following configurations:
+
+* (Required) `{app, string()}`: Defines an `app` entry which is included in all 
+    log messages.
+* (Optional) `{defaults, [{string(), string()}]}`: Set other default entries
+    which will occurr in all log messages.
+
+A lager configuration example looks like this:
+
+```
+{lager, {
+    [{handlers, [
+        {lager_console_backend, info, {lager_elogfmt_formatter, [{app, "myapp"},
+                                                                 {defaults, [{"key", "value"}]}
+                                                                ]}
+        }]
+    }]}
+}
+```
+
+# Test
+
+Run the unit and type checker:
+
+```
+$ rebar3 eunit, dialyzer
+```
+
---
+++ src/lager_elogfmt_formatter.erl
@@ -0,0 +1,203 @@
+-module('lager_elogfmt_formatter').
+
+%% API exports
+-export([format/2,
+         format/3
+        ]).
+
+-type default_entry() :: {string(), string()}.
+-type config() :: [{app, string()} | {defaults, [default_entry()]}].
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+-spec format(lager:msg(), config(), list()) -> elogfmt_core:logmessage().
+format(Msg, Config, _Color) ->
+    %% this formatter ignores color
+    format(Msg, Config).
+
+-spec format(lager:msg(), config()) -> elogfmt_core:logmessage().
+format(Msg, Config) ->
+    App = proplists:get_value(app, Config),
+    Defaults = proplists:get_value(defaults, Config, []),
+    Props = filter_undefined([{"app", App},
+                              severity(Msg),
+                              msg(Msg) |
+                              Defaults ++
+                              meta(Msg, App)
+                             ]),
+    elogfmt_core:logmessage(Props) ++ "\n".
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+severity(Msg) ->
+    Severity = lager_msg:severity(Msg),
+    {"severity", atom_to_list(Severity)}.
+
+msg(Msg) ->
+    Message = escape(lager_msg:message(Msg)),
+    {"msg", ["'", Message, "'"]}.
+
+meta(Msg, App) ->
+    Meta = lager_msg:metadata(Msg),
+    transform_meta(Meta, App, []).
+
+transform_meta([], _App, Acc) ->
+    Acc;
+transform_meta([{pid, _Pid} | Rest], App, Acc) ->
+    %% ignore pid
+    transform_meta(Rest, App, Acc);
+transform_meta([{node, Node} | Rest], App, Acc) ->
+    transform_meta(Rest, App, [{"node", atom_to_list(Node)} | Acc]);
+transform_meta([{application, Application} | Rest], App, Acc) ->
+    transform_meta(Rest, App,
+                   [{"application", atom_to_list(Application)} | Acc]);
+transform_meta([{module, Mod} | Rest], App, Acc) ->
+    transform_meta(Rest, App, [{"module", atom_to_list(Mod)} | Acc]);
+transform_meta([{function, Fun} | Rest], App, Acc) ->
+    transform_meta(Rest, App, [{"function", atom_to_list(Fun)} | Acc]);
+transform_meta([{{count, Key}, Value} | Rest], App, Acc) ->
+    MetricKey = ["count#", App, ".", Key],
+    transform_meta(Rest, App, [{MetricKey, Value},
+                               {splunk_key(Key), Value} | Acc]);
+transform_meta([{{measure, Key}, Value} | Rest], App, Acc) ->
+    MetricKey = ["measure#", App, ".", Key],
+    transform_meta(Rest, App, [{MetricKey, Value},
+                               {splunk_key(Key), Value} | Acc]);
+transform_meta([{{sample, Key}, Value} | Rest], App, Acc) ->
+    MetricKey = ["sample#", App, ".", Key],
+    transform_meta(Rest, App, [{MetricKey, Value},
+                               {splunk_key(Key), Value} | Acc]);
+transform_meta([{{unique, Key}, Value} | Rest], App, Acc) ->
+    MetricKey = ["unique#", App, ".", Key],
+    transform_meta(Rest, App, [{MetricKey, Value},
+                               {splunk_key(Key), Value} | Acc]);
+transform_meta([{_Key, undefined} | Rest], App, Acc) ->
+    %% filter undefined values
+    transform_meta(Rest, App, Acc);
+transform_meta([{Key, Value} | Rest], App, Acc) when is_atom(Value) ->
+    transform_meta(Rest, App, [{splunk_key(Key), atom_to_list(Value)} | Acc]);
+transform_meta([{Key, Value} | Rest], App, Acc) ->
+    transform_meta(Rest, App, [{splunk_key(Key), Value} | Acc]).
+
+splunk_key(Key) when is_atom(Key) ->
+    splunk_key(atom_to_list(Key));
+splunk_key(Key) ->
+    re:replace(Key, "-", "_", [global, {return, list}]).
+
+filter_undefined(Props) ->
+    [{K, V} || {K, V} <- Props, V =/= undefined].
+
+escape(Msg) ->
+    lists:foldr(fun escape_char/2, [], Msg).
+
+escape_char($', Acc) ->
+    [$\\, $' | Acc];
+escape_char(Other, Acc) ->
+    [Other| Acc].
+
+%%====================================================================
+%% tests
+%%====================================================================
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+severity_test() ->
+    Msg = lager_msg:new("msg", error, [], []),
+    ?assertEqual({"severity", "error"}, severity(Msg)).
+
+msg_test() ->
+    Msg = lager_msg:new("'msg'", error, [], []),
+    ?assertEqual({"msg", ["'", "\\'msg\\'", "'"]}, msg(Msg)).
+
+meta_ignore_pid_test() ->
+    Msg = lager_msg:new("msg", error, [{pid, list_to_pid("<0.1.0>")}], []),
+    ?assertEqual([], meta(Msg, "myapp")).
+
+meta_node_test() ->
+    Msg = lager_msg:new("msg", error, [{node, node()}], []),
+    ?assertEqual([{"node", atom_to_list(node())}], meta(Msg, "myapp")).
+
+meta_application_test() ->
+    Msg = lager_msg:new("msg", error, [{application, myapp}], []),
+    ?assertEqual([{"application", "myapp"}], meta(Msg, "myapp")).
+
+meta_module_test() ->
+    Msg = lager_msg:new("msg", error, [{module, mymod}], []),
+    ?assertEqual([{"module", "mymod"}], meta(Msg, "myapp")).
+
+meta_function_test() ->
+    Msg = lager_msg:new("msg", error, [{function, myfun}], []),
+    ?assertEqual([{"function", "myfun"}], meta(Msg, "myapp")).
+
+meta_count_test() ->
+    Msg = lager_msg:new("msg", error, [{{count, "my_count"}, 42}], []),
+    ?assertEqual([{["count#", "myapp", ".", "my_count"], 42},
+                  {"my_count", 42}],
+                 meta(Msg, "myapp")).
+
+meta_measure_test() ->
+    Msg = lager_msg:new("msg", error, [{{measure, "my_measure"}, 23}], []),
+    ?assertEqual([{["measure#", "myapp", ".", "my_measure"], 23},
+                  {"my_measure", 23}],
+                 meta(Msg, "myapp")).
+
+meta_sample_test() ->
+    Msg = lager_msg:new("msg", error, [{{sample, "my_sample"}, 1}], []),
+    ?assertEqual([{["sample#", "myapp", ".", "my_sample"], 1},
+                  {"my_sample", 1}],
+                 meta(Msg, "myapp")).
+
+meta_unique_test() ->
+    Msg = lager_msg:new("msg", error, [{{unique, "my_unique"}, 1234}], []),
+    ?assertEqual([{["unique#", "myapp", ".", "my_unique"], 1234},
+                  {"my_unique", 1234}],
+                 meta(Msg, "myapp")).
+
+generic_meta_atom_value_test() ->
+    Msg = lager_msg:new("msg", error, [{key, value}], []),
+    ?assertEqual([{"key", "value"}], meta(Msg, "myapp")).
+
+generic_meta_dashed_key_test() ->
+    Msg = lager_msg:new("msg", error, [{"k-e-y", "value"}], []),
+    ?assertEqual([{"k_e_y", "value"}], meta(Msg, "myapp")).
+
+format_test() ->
+    Msg = lager_msg:new("'msg'", error, [{application, myapp},
+                                         {module, mymod},
+                                         {function, myfun},
+                                         {line, 100},
+                                         {{count, "mycount"}, 1},
+                                         {{measure, "mymeasure"}, 23},
+                                         {{unique, "myunique"}, 1234},
+                                         {{sample, "mysample"}, 42},
+                                         {key, value},
+                                         {"dashed-key", "value"},
+                                         {undefined_key, undefined}],
+                        []),
+    Config = [{app, "myapp"}, {defaults, [{"default", "value"}]}],
+    ?assertEqual(<<"app=myapp "
+                   "severity=error "
+                   "msg='\\'msg\\'' "
+                   "default=value "
+                   "dashed_key=value "
+                   "key=value "
+                   "sample#myapp.mysample=42 "
+                   "mysample=42 "
+                   "unique#myapp.myunique=1234 "
+                   "myunique=1234 "
+                   "measure#myapp.mymeasure=23 "
+                   "mymeasure=23 "
+                   "count#myapp.mycount=1 "
+                   "mycount=1 "
+                   "line=100 "
+                   "function=myfun "
+                   "module=mymod "
+                   "application=myapp"
+                   "\n">>,
+                 list_to_binary(format(Msg, Config))).
+
+-endif.
